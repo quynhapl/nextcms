d11033b41a892d7571b6f52cc9c1524d08dcf7df
Nguyen Huu Phuoc <thenextcms@gmail.com>
2012-07-09 11:35:18 +0700

    #52 [file] Showing the progress when upload files
    Change-Id: I0000000000000000000000000000000000000000

--- modules/file/controllers/FileController.php
+++ modules/file/controllers/FileController.php
@@ -10,7 +10,7 @@
  * @package		file
  * @subpackage	controllers
  * @since		1.0
- * @version		2012-07-02
+ * @version		2012-07-09
  */
 
 defined('APP_VALID_REQUEST') || die('You cannot access the script directly.');
@@ -31,15 +31,39 @@ class File_FileController extends Zend_Controller_Action
 		$module    = $request->getParam('mod', 'file');
 		$thumbnail = $request->getParam('thumbnail', 'false');
 		$watermark = $request->getParam('watermark', 'false');
+		$uploader  = $request->getParam('uploader', 'flash');
 		
-		// I use Dojo Uploader in the client side
-		// and it automatically appends the "s" at the end of name
-		$fileName  = $request->getParam('name', 'uploadedfile') . 's';
-		$files	   = File_Services_Uploader::upload($fileName, $module, array(
-			'thumbnail' => $thumbnail == 'true',
-			'watermark' => $watermark == 'true',
-		 ));
-		
-		$this->_helper->json($files);
+		switch ($uploader) {
+			case 'html5':
+				// Dojo HTML5 uploader automatically appends the "s" at the end of name
+				$fileName = $request->getParam('name', 'uploadedfile') . 's';
+				$files	  = File_Services_Uploader::upload($fileName, $module, array(
+					'thumbnail' => 'true' == $thumbnail,
+					'watermark' => 'true' == $watermark,
+				));
+				$this->_helper->json($files);
+				break;
+			case 'flash':
+			default:
+				// Use Flash to upload files
+				$this->getResponse()->setHeader('Content-Type', 'text/html; charset=utf-8');
+				$this->_helper->getHelper('layout')->disableLayout();
+				$this->_helper->getHelper('viewRenderer')->setNoRender();
+				
+				$fileName = $request->getParam('name', 'uploadedfile');
+				$files	  = File_Services_Uploader::upload($fileName, $module, array(
+					'thumbnail' => 'true' == $thumbnail,
+					'watermark' => 'true' == $watermark,
+				));
+				$return	  = array(
+					'name=' . (isset($files[0]['original']['name'])		 ? $files[0]['original']['name']	  : ''),
+					'type=' . (isset($files[0]['original']['extension']) ? $files[0]['original']['extension'] : ''),
+					'size=' . (isset($files[0]['original']['size'])		 ? $files[0]['original']['size']	  : 0),
+					'appUploadedFiles=' . rawurlencode(base64_encode(Zend_Json::encode($files))),
+				);
+				$return = implode(',', $return);
+				$this->getResponse()->setBody($return);
+				break;
+		}
 	}
 }
