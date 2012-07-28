3ad7706c080de3c6b8f27f4ea46621fc0363e39c
Nguyen Huu Phuoc <thenextcms@gmail.com>
2012-07-28 10:58:30 +0700

    [core] Upgrade Dojo to 1.7.3 for the errors managing page
    Change-Id: Ib30d91e9dc9ded09543132a399f8b9337d5b70d8

--- modules/core/js/controllers/ErrorController.js
+++ modules/core/js/controllers/ErrorController.js
@@ -9,189 +9,197 @@
  * @package		core
  * @subpackage	js
  * @since		1.0
- * @version		2012-05-16
+ * @version		2012-07-28
  */
 
-dojo.provide("core.js.controllers.ErrorController");
-
-dojo.require("core.js.base.controllers.ActionProvider");
-dojo.require("core.js.base.controllers.Subscriber");
-dojo.require("core.js.base.Encoder");
-dojo.require("core.js.base.I18N");
-dojo.require("core.js.base.views.Helper");
-
-dojo.declare("core.js.controllers.ErrorController", null, {
-	// _id: String
-	_id: null,
-	
-	// _i18n: Object
-	_i18n: null,
-	
-	// _helper: core.js.base.views.Helper
-	_helper: null,
-	
-	// _errorToolbar: core.js.views.ErrorToolbar
-	_errorToolbar: null,
-	
-	// _errorGrid: core.js.views.ErrorGrid
-	_errorGrid: null,
-	
-	// _paginatorContainer: String
-	_paginatorContainer: null,
-	
-	// _defaultCriteria: Object
-	_defaultCriteria: {
-		page: 1,
-		module: null,
-		from_date: null,
-		to_date: null
-	},
-	
-	// TOPIC_GROUP: [const] String
-	TOPIC_GROUP: "/core/js/controllers/ErrorController",
-	
-	constructor: function(/*String*/ id) {
-		this._id = id;
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/xhr",
+	"dojo/aspect",
+	"dojo/io-query",
+	"dojo/topic",
+	"dojo/hash",
+	"dijit/registry",
+	"core/js/base/controllers/ActionProvider",
+	"core/js/base/controllers/Subscriber",
+	"core/js/base/Encoder",
+	"core/js/base/I18N",
+	"core/js/base/views/Helper"
+], function(dojoDeclare, dojoLang, dojoXhr, dojoAspect, dojoIoQuery, dojoTopic) {
+	return dojoDeclare("core.js.controllers.ErrorController", null, {
+		// _id: String
+		_id: null,
 		
-		core.js.base.I18N.requireLocalization("core/languages");
-		this._i18n = core.js.base.I18N.getLocalization("core/languages");
+		// _i18n: Object
+		_i18n: null,
 		
-		// Create helper instance
-		this._helper = new core.js.base.views.Helper(id);
-		this._helper.setLanguageData(this._i18n);
+		// _helper: core.js.base.views.Helper
+		_helper: null,
 		
-		core.js.base.controllers.Subscriber.unsubscribe(this.TOPIC_GROUP);
-	},
-	
-	setErrorToolbar: function(/*core.js.views.ErrorToolbar*/ toolbar) {
-		// summary:
-		//		Sets the error toolbar
-		this._errorToolbar = toolbar;
+		// _errorToolbar: core.js.views.ErrorToolbar
+		_errorToolbar: null,
 		
-		// Refresh handler
-		dojo.connect(toolbar, "onRefresh", this, "searchErrors");
+		// _errorGrid: core.js.views.ErrorGrid
+		_errorGrid: null,
 		
-		// Module filter handler
-		dojo.connect(toolbar, "onSelectModule", this, function(module) {
-			this.searchErrors({
-				module: module
-			});
-		});
+		// _paginatorContainer: String
+		_paginatorContainer: null,
 		
-		// Search handler
-		dojo.connect(toolbar, "onSearchErrors", this, function(criteria) {
-			this.searchErrors(criteria);
-		});
+		// _defaultCriteria: Object
+		_defaultCriteria: {
+			page: 1,
+			module: null,
+			from_date: null,
+			to_date: null
+		},
 		
-		return this;	// core.js.controllers.ErrorController
-	},
-	
-	setErrorGrid: function(/*core.js.views.ErrorGrid*/ grid) {
-		// summary:
-		//		Sets the error grid
-		this._errorGrid = grid;
+		// TOPIC_GROUP: [const] String
+		TOPIC_GROUP: "/core/js/controllers/ErrorController",
 		
-		// Delete error handler
-		dojo.connect(grid, "onDeleteError", this, "deleteError");
-		core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/delete/onCancel", this, function() {
-			this._helper.closeDialog();
-		});
-		core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/delete/onComplete", this, function(data) {
-			this._helper.closeDialog();
-			dojo.publish("/app/global/notification", [{
-				message: this._i18n.error["delete"][(data.result == "APP_RESULT_OK") ? "success" : "error"],
-				type: (data.result == "APP_RESULT_OK") ? "message" : "error"
-			}]);
+		constructor: function(/*String*/ id) {
+			this._id = id;
 			
-			if (data.result == "APP_RESULT_OK") {
-				this.searchErrors();
-			}
-		});
+			core.js.base.I18N.requireLocalization("core/languages");
+			this._i18n = core.js.base.I18N.getLocalization("core/languages");
+			
+			// Create helper instance
+			this._helper = new core.js.base.views.Helper(id);
+			this._helper.setLanguageData(this._i18n);
+			
+			core.js.base.controllers.Subscriber.unsubscribe(this.TOPIC_GROUP);
+		},
 		
-		// View error handler
-		dojo.connect(grid, "onViewError", this, "viewError");
-		core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/view/onCancel", this, function() {
-			this._helper.removePane();
-		});
+		setErrorToolbar: function(/*core.js.views.ErrorToolbar*/ toolbar) {
+			// summary:
+			//		Sets the error toolbar
+			this._errorToolbar = toolbar;
+			
+			var that = this;
+			// Refresh handler
+			dojoAspect.after(toolbar, "onRefresh", dojoLang.hitch(this, "searchErrors"));
+			
+			// Module filter handler
+			dojoAspect.after(toolbar, "onSelectModule", function(module) {
+				that.searchErrors({
+					module: module
+				});
+			}, true);
+			
+			// Search handler
+			dojoAspect.after(toolbar, "onSearchErrors", dojoLang.hitch(this, "searchErrors"), true);
+			
+			return this;	// core.js.controllers.ErrorController
+		},
 		
-		return this;	// core.js.controllers.ErrorController
-	},
-	
-	setErrorPaginator: function(/*String*/ paginatorContainer) {
-		// summary:
-		//		Sets the container of paginator
-		this._paginatorContainer = paginatorContainer;
-		
-		core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/list/onGotoPage", this, function(page) {
-			this.searchErrors({
-				page: page
+		setErrorGrid: function(/*core.js.views.ErrorGrid*/ grid) {
+			// summary:
+			//		Sets the error grid
+			this._errorGrid = grid;
+			
+			// Delete error handler
+			dojoAspect.after(grid, "onDeleteError", dojoLang.hitch(this, "deleteError"), true);
+			core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/delete/onCancel", this, function() {
+				this._helper.closeDialog();
 			});
-		});
-		
-		return this;	// core.js.controllers.ErrorController
-	},
-	
-	deleteError: function(/*dojo.data.Item*/ item) {
-		// summary:
-		//		Deletes the given error
-		var params = {
-			error_id: item.error_id[0]
-		};
-		var url = core.js.base.controllers.ActionProvider.get("core_error_delete").url + "?" + dojo.objectToQuery(params);
-		this._helper.showDialog(url, {
-			title: this._i18n.error["delete"].title,
-			style: "width: 250px",
-			refreshOnShow: true
-		});
-	},
-	
-	initSearchCriteria: function(/*Object*/ criteria) {
-		// summary:
-		//		Inits with given criteria
-		dojo.mixin(this._defaultCriteria, criteria);
-		this._errorToolbar.initSearchCriteria(this._defaultCriteria);
-		return this;	// core.js.controllers.ErrorController
-	},
-	
-	searchErrors: function(/*Object*/ criteria) {
-		// summary:
-		//		Searches for errors
-		dojo.mixin(this._defaultCriteria, criteria);
-		
-		var q   = core.js.base.Encoder.encode(this._defaultCriteria);
-		var url = core.js.base.controllers.ActionProvider.get("core_error_list").url;
-		dojo.hash("u=" + url + "/?q=" + q);
-		
-		var _this = this;
-		this._helper.showStandby();
-		dojo.xhrPost({
-			url: url,
-			content: {
-				q: q,
-				format: "json"
-			},
-			handleAs: "json",
-			load: function(data) {
-				_this._helper.hideStandby();
-				_this._errorGrid.showErrors(data.errors);
+			core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/delete/onComplete", this, function(data) {
+				this._helper.closeDialog();
+				dojoTopic.publish("/app/global/notification", {
+					message: this._i18n.error["delete"][("APP_RESULT_OK" == data.result) ? "success" : "error"],
+					type: ("APP_RESULT_OK" == data.result) ? "message" : "error"
+				});
 				
-				// Update the paginator
-				dijit.byId(_this._paginatorContainer).setContent(data.paginator);
-			}
-		});
+				if ("APP_RESULT_OK" == data.result) {
+					this.searchErrors();
+				}
+			});
+			
+			// View error handler
+			dojoAspect.after(grid, "onViewError", dojoLang.hitch(this, "viewError"), true);
+			core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/view/onCancel", this, function() {
+				this._helper.removePane();
+			});
+			
+			return this;	// core.js.controllers.ErrorController
+		},
 		
-		return this;	// core.js.controllers.ErrorController
-	},
-	
-	viewError: function(/*dojo.data.Item*/ item) {
-		// summary:
-		//		Views given error item
-		var params = {
-			error_id: item.error_id[0]
-		};
-		var url = core.js.base.controllers.ActionProvider.get("core_error_view").url + "?" + dojo.objectToQuery(params);
-		this._helper.showPane(url, {
-			style: "width: 50%"
-		});
-	}
+		setErrorPaginator: function(/*String*/ paginatorContainer) {
+			// summary:
+			//		Sets the container of paginator
+			this._paginatorContainer = paginatorContainer;
+			
+			core.js.base.controllers.Subscriber.subscribe(this.TOPIC_GROUP, "/app/core/error/list/onGotoPage", this, function(page) {
+				this.searchErrors({
+					page: page
+				});
+			});
+			
+			return this;	// core.js.controllers.ErrorController
+		},
+		
+		deleteError: function(/*dojo.data.Item*/ item) {
+			// summary:
+			//		Deletes the given error
+			var params = {
+				error_id: item.error_id[0]
+			};
+			var url = core.js.base.controllers.ActionProvider.get("core_error_delete").url + "?" + dojoIoQuery.objectToQuery(params);
+			this._helper.showDialog(url, {
+				id: "coreErrorDeleteConfirmDialog",
+				refreshOnShow: true,
+				style: "width: 250px",
+				title: this._i18n.error["delete"].title
+			});
+		},
+		
+		initSearchCriteria: function(/*Object*/ criteria) {
+			// summary:
+			//		Inits with given criteria
+			dojoLang.mixin(this._defaultCriteria, criteria);
+			this._errorToolbar.initSearchCriteria(this._defaultCriteria);
+			return this;	// core.js.controllers.ErrorController
+		},
+		
+		searchErrors: function(/*Object*/ criteria) {
+			// summary:
+			//		Searches for errors
+			dojoLang.mixin(this._defaultCriteria, criteria);
+			
+			var q   = core.js.base.Encoder.encode(this._defaultCriteria);
+			var url = core.js.base.controllers.ActionProvider.get("core_error_list").url;
+			dojo.hash("u=" + url + "/?q=" + q);
+			
+			var that = this;
+			this._helper.showStandby();
+			dojoXhr.post({
+				url: url,
+				content: {
+					q: q,
+					format: "json"
+				},
+				handleAs: "json",
+				load: function(data) {
+					that._helper.hideStandby();
+					that._errorGrid.showErrors(data.errors);
+					
+					// Update the paginator
+					dijit.registry.byId(that._paginatorContainer).set("content", data.paginator);
+				}
+			});
+			
+			return this;	// core.js.controllers.ErrorController
+		},
+		
+		viewError: function(/*dojo.data.Item*/ item) {
+			// summary:
+			//		Views given error item
+			var params = {
+				error_id: item.error_id[0]
+			};
+			var url = core.js.base.controllers.ActionProvider.get("core_error_view").url + "?" + dojoIoQuery.objectToQuery(params);
+			this._helper.showPane(url, {
+				style: "width: 50%"
+			});
+		}
+	});
 });
--- modules/core/js/views/ErrorToolbar.js
+++ modules/core/js/views/ErrorToolbar.js
@@ -9,179 +9,180 @@
  * @package		core
  * @subpackage	js
  * @since		1.0
- * @version		2011-10-18
+ * @version		2012-07-28
  */
 
-dojo.provide("core.js.views.ErrorToolbar");
+define([
+	"dojo/_base/declare",
+	"dojo/parser",
+	"dijit/form/Button",
+	"dijit/form/DateTextBox",
+	"dijit/form/DropDownButton",
+	"dijit/Menu",
+	"dijit/MenuItem",
+	"dijit/MenuSeparator",
+	"dijit/Toolbar",
+	"dijit/ToolbarSeparator",
+	"dojo/date/stamp",
+	"core/js/base/controllers/ActionProvider",
+	"core/js/base/I18N"
+], function(dojoDeclare) {
+	return dojoDeclare("core.js.views.ErrorToolbar", null, {
+		// _id: String
+		_id: null,
 
-dojo.require("dijit.form.Button");
-dojo.require("dijit.form.DateTextBox");
-dojo.require("dijit.form.DropDownButton");
-dojo.require("dijit.Menu");
-dojo.require("dijit.MenuItem");
-dojo.require("dijit.MenuSeparator");
-dojo.require("dijit.Toolbar");
-dojo.require("dijit.ToolbarSeparator");
-dojo.require("dojo.date.stamp");
-
-dojo.require("core.js.base.controllers.ActionProvider");
-dojo.require("core.js.base.I18N");
-
-dojo.declare("core.js.views.ErrorToolbar", null, {
-	// _id: String
-	_id: null,
-
-	// _i18n: Object
-	_i18n: null,
-	
-	// _moduleDropDownButton: dijit.form.DropDownButton
-	_moduleDropDownButton: null,
-	
-	// _fromDateTextBox: dijit.form.DateTextBox
-	_fromDateTextBox: null,
-	
-	// _toDateTextBox: dijit.form.DateTextBox
-	_toDateTextBox: null,
-	
-	// _modules: Array
-	_modules: [],
-	
-	constructor: function(/*String*/ id) {
-		this._id = id;
+		// _i18n: Object
+		_i18n: null,
 		
-		core.js.base.I18N.requireLocalization("core/languages");
-		this._i18n = core.js.base.I18N.getLocalization("core/languages");
-	},
-	
-	setModules: function(/*Array*/ modules) {
-		// summary:
-		//		Sets array of modules
-		this._modules = modules;
-		return this;	// core.js.views.ErrorToolbar
-	},
-	
-	show: function() {
-		// summary:
-		//		Shows the toolbar
-		var _this = this;
-		var toolbar = new dijit.Toolbar({}, this._id);
+		// _moduleDropDownButton: dijit.form.DropDownButton
+		_moduleDropDownButton: null,
 		
-		// "Refresh" button
-		this._refreshButton = new dijit.form.Button({
-			label: this._i18n.global._share.refreshAction,
-			showLabel: false,
-			iconClass: "appIcon appRefreshIcon",
-			disabled: !core.js.base.controllers.ActionProvider.get("core_error_list").isAllowed,
-			onClick: function(e) {
-				_this.onRefresh();
-			}
-		});
-		toolbar.addChild(this._refreshButton);
+		// _fromDateTextBox: dijit.form.DateTextBox
+		_fromDateTextBox: null,
 		
-		if (this._modules.length > 0) {
-			// Add a module filter
-			var modulesMenu = new dijit.Menu();
-			modulesMenu.addChild(new dijit.MenuItem({
-				label: this._i18n.error.list.allModules,
-				onClick: function(e) {
-					_this._moduleDropDownButton.set("label", this.label);
-					_this.onSelectModule(null);
-				}
-			}));
-			modulesMenu.addChild(new dijit.MenuSeparator());
+		// _toDateTextBox: dijit.form.DateTextBox
+		_toDateTextBox: null,
+		
+		// _modules: Array
+		_modules: [],
+		
+		constructor: function(/*String*/ id) {
+			this._id = id;
 			
-			for (var i in this._modules) {
+			core.js.base.I18N.requireLocalization("core/languages");
+			this._i18n = core.js.base.I18N.getLocalization("core/languages");
+		},
+		
+		setModules: function(/*Array*/ modules) {
+			// summary:
+			//		Sets array of modules
+			this._modules = modules;
+			return this;	// core.js.views.ErrorToolbar
+		},
+		
+		show: function() {
+			// summary:
+			//		Shows the toolbar
+			var that = this;
+			var toolbar = new dijit.Toolbar({}, this._id);
+			
+			// "Refresh" button
+			this._refreshButton = new dijit.form.Button({
+				label: this._i18n.global._share.refreshAction,
+				showLabel: false,
+				iconClass: "appIcon appRefreshIcon",
+				disabled: !core.js.base.controllers.ActionProvider.get("core_error_list").isAllowed,
+				onClick: function(e) {
+					that.onRefresh();
+				}
+			});
+			toolbar.addChild(this._refreshButton);
+			
+			if (this._modules.length > 0) {
+				// Add a module filter
+				var modulesMenu = new dijit.Menu();
 				modulesMenu.addChild(new dijit.MenuItem({
-					__module: this._modules[i].name,
-					label: this._modules[i].title,
+					label: this._i18n.error.list.allModules,
 					onClick: function(e) {
-						_this._moduleDropDownButton.set("label", this.label);
-						_this.onSelectModule(this.__module);
+						that._moduleDropDownButton.set("label", this.label);
+						that.onSelectModule(null);
 					}
 				}));
-			}
-			toolbar.addChild(new dijit.ToolbarSeparator());
-			this._moduleDropDownButton = new dijit.form.DropDownButton({
-				label: this._i18n.error.list.allModules,
-				showLabel: true,
-				dropDown: modulesMenu
-			});
-			toolbar.addChild(this._moduleDropDownButton);
-		}
-		
-		toolbar.addChild(new dijit.ToolbarSeparator());
-		// Search for errors in a range of dates
-		this._fromDateTextBox = new dijit.form.DateTextBox({
-			style: "margin: 0 5px; width: 100px",
-			placeHolder: this._i18n.error.list.fromDate
-		});
-		toolbar.addChild(this._fromDateTextBox);
-		
-		this._toDateTextBox = new dijit.form.DateTextBox({
-			style: "width: 100px",
-			placeHolder: this._i18n.error.list.toDate
-		});
-		toolbar.addChild(this._toDateTextBox);
-		
-		toolbar.addChild(new dijit.form.Button({
-			label: this._i18n.global._share.searchAction,
-			showLabel: false,
-			iconClass: "appIcon appSearchIcon",
-			disabled: !core.js.base.controllers.ActionProvider.get("core_error_list").isAllowed,
-			onClick: function(e) {
-				var fromDate = _this._fromDateTextBox.get("value");
-				var toDate   = _this._toDateTextBox.get("value");
+				modulesMenu.addChild(new dijit.MenuSeparator());
 				
-				if (fromDate) {
-					// DOJO LESSON: Format the date provided by a dijit.form.DateTextBox widget
-					fromDate = dojo.date.stamp.toISOString(fromDate, {selector: "date"});
+				for (var i in this._modules) {
+					modulesMenu.addChild(new dijit.MenuItem({
+						appModule: this._modules[i].name,
+						label: this._modules[i].title,
+						onClick: function(e) {
+							that._moduleDropDownButton.set("label", this.label);
+							that.onSelectModule(this.appModule);
+						}
+					}));
 				}
-				if (toDate) {
-					toDate = dojo.date.stamp.toISOString(toDate, {selector: "date"});
-				}
-				_this.onSearchErrors({
-					from_date: fromDate,
-					to_date: toDate
+				toolbar.addChild(new dijit.ToolbarSeparator());
+				this._moduleDropDownButton = new dijit.form.DropDownButton({
+					label: this._i18n.error.list.allModules,
+					showLabel: true,
+					dropDown: modulesMenu
 				});
+				toolbar.addChild(this._moduleDropDownButton);
 			}
-		}));
-	},
-	
-	initSearchCriteria: function(/*Object*/ criteria) {
-		// summary:
-		//		Init controls with given criteria
-		if (criteria.from_date) {
-			this._fromDateTextBox.set("value", criteria.from_date);
+			
+			toolbar.addChild(new dijit.ToolbarSeparator());
+			// Search for errors in a range of dates
+			this._fromDateTextBox = new dijit.form.DateTextBox({
+				style: "margin: 0 5px; width: 100px",
+				placeHolder: this._i18n.error.list.fromDate
+			});
+			toolbar.addChild(this._fromDateTextBox);
+			
+			this._toDateTextBox = new dijit.form.DateTextBox({
+				style: "width: 100px",
+				placeHolder: this._i18n.error.list.toDate
+			});
+			toolbar.addChild(this._toDateTextBox);
+			
+			toolbar.addChild(new dijit.form.Button({
+				label: this._i18n.global._share.searchAction,
+				showLabel: false,
+				iconClass: "appIcon appSearchIcon",
+				disabled: !core.js.base.controllers.ActionProvider.get("core_error_list").isAllowed,
+				onClick: function(e) {
+					var fromDate = that._fromDateTextBox.get("value");
+					var toDate   = that._toDateTextBox.get("value");
+					
+					if (fromDate) {
+						// DOJO LESSON: Format the date provided by a dijit.form.DateTextBox widget
+						fromDate = dojo.date.stamp.toISOString(fromDate, { selector: "date" });
+					}
+					if (toDate) {
+						toDate = dojo.date.stamp.toISOString(toDate, { selector: "date" });
+					}
+					that.onSearchErrors({
+						from_date: fromDate,
+						to_date: toDate
+					});
+				}
+			}));
+		},
+		
+		initSearchCriteria: function(/*Object*/ criteria) {
+			// summary:
+			//		Init controls with given criteria
+			if (criteria.from_date) {
+				this._fromDateTextBox.set("value", criteria.from_date);
+			}
+			if (criteria.to_date) {
+				this._toDateTextBox.set("value", criteria.to_date);
+			}
+		},
+		
+		////////// CALLBACKS //////////
+		
+		onRefresh: function() {
+			// summary:
+			//		Reloads the list of errors
+			// tags:
+			//		callback
+		},
+		
+		onSearchErrors: function(/*Object*/ criteria) {
+			// summary:
+			//		Searches for errors by given criteria
+			// criteria:
+			//		Consists of the following members:
+			//		- from_date
+			//		- to_date
+			// tags:
+			//		callback
+		},
+		
+		onSelectModule: function(/*String?*/ module) {
+			// summary:
+			//		Loads the list of errors in given module
+			// tags:
+			//		callback
 		}
-		if (criteria.to_date) {
-			this._toDateTextBox.set("value", criteria.to_date);
-		}
-	},
-	
-	////////// CALLBACKS //////////
-	
-	onRefresh: function() {
-		// summary:
-		//		Reloads the list of errors
-		// tags:
-		//		callback
-	},
-	
-	onSearchErrors: function(/*Object*/ criteria) {
-		// summary:
-		//		Searches for errors by given criteria
-		// criteria:
-		//		Consists of the following members:
-		//		- from_date
-		//		- to_date
-		// tags:
-		//		callback
-	},
-	
-	onSelectModule: function(/*String?*/ module) {
-		// summary:
-		//		Loads the list of errors in given module
-		// tags:
-		//		callback
-	}
+	});
 });
